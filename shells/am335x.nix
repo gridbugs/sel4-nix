f46988f7c6156fc71ea48f8f3c1d78dc8fc259bd
Stephen Sherratt <stephen@sherra.tt>
2022-11-15 21:11:37 +1100

    Improve separation between native and cross packages

--- shells/am335x.nix
+++ shells/am335x.nix
@@ -1,18 +1,21 @@
-{ pkgs ? import <nixpkgs> {
-  crossSystem = { config = "arm-none-eabi"; };
-} }:
+{
+  pkgsNative ? import <nixpkgs> {},
+  pkgsCross ? import <nixpkgs> {
+    crossSystem = { config = "arm-none-eabi"; };
+  },
+}:
 let
   # pyfdt isn't in the nixpkgs repo
-  pyfdt = pkgs.buildPackages.python3Packages.buildPythonPackage {
+  pyfdt = pkgsNative.python3Packages.buildPythonPackage {
     name = "pydft";
-    src = pkgs.python3Packages.fetchPypi {
+    src = pkgsNative.python3Packages.fetchPypi {
       pname = "pyfdt";
       version = "0.3";
       sha256 = "sha256:1w7lp421pssfgv901103521qigwb12i6sk68lqjllfgz0lh1qq31";
     };
     propagatedBuildInputs = [];
   };
-  pythonWithPackages = pkgs.buildPackages.python3.withPackages (pkg: [
+  pythonWithPackages = pkgsNative.python3.withPackages (pkg: [
     pkg.six
     pkg.future
     pkg.setuptools
@@ -25,23 +28,25 @@ let
     pkg.jsonschema
   ]);
 in
-pkgs.mkShell {
+pkgsNative.mkShell {
   nativeBuildInputs = [
     pythonWithPackages
     pyfdt
-    pkgs.buildPackages.git
-    pkgs.buildPackages.gitRepo
-    pkgs.buildPackages.cacert
-    pkgs.buildPackages.cmake
-    pkgs.buildPackages.ninja
-    pkgs.buildPackages.libxml2
-    pkgs.buildPackages.protobuf
-    pkgs.buildPackages.binutils
-    pkgs.buildPackages.gcc
-    pkgs.buildPackages.dtc
-    pkgs.buildPackages.cpio
-    pkgs.buildPackages.ubootTools
-    pkgs.buildPackages.qemu
-    pkgs.buildPackages.ncurses
-  ];
+  ] ++ (with pkgsNative; [
+    git
+    gitRepo
+    cacert
+    cmake
+    ninja
+    libxml2
+    protobuf
+    dtc
+    cpio
+    ubootTools
+    qemu
+    ncurses
+  ]) ++ (with pkgsCross.buildPackages; [
+    binutils
+    gcc
+  ]);
 }
